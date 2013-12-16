1aed1e34d8a51f210d9762c78d79ac9313cae03b
Michael Barrett <mb@abouthalf.com>
2013-12-15 19:54:09 -0800

    attempting html entities conversion

--- web/index.php
+++ web/index.php
@@ -104,9 +104,10 @@ function getPageContent($page)
 		throw new Exception('content file not found');
 	}
 	$contents = mb_convert_encoding($contents,'utf-8',mb_detect_encoding($contents));
+	$content = mb_convert_encoding($contents, 'html-entities','utf-8');
 	$document = new DOMDocument('1.0','UTF-8');
 	$document->substituteEntities = true;
-	$document->loadHTML($contents, LIBXML_NOERROR);
+	@$document->loadHTML($contents, LIBXML_NOERROR);
 	$xml = simplexml_import_dom($document);
 //	$h = @fopen($f,'rb');
 //	if ($h === false) {
